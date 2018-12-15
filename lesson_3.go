6bd5fb3db7109488d445ddff0080414d5b2906e3
Steve H <1137201+schigh@users.noreply.github.com>
2018-12-15 10:02:56 -0500

    Update lesson_3.go
    added incomplete goplay link

--- lesson_3.go
+++ lesson_3.go
@@ -14,6 +14,7 @@ func right() {
 	fmt.Println("right")
 }
 // This will draw a blue box
+// incomplete: https://goplay.space/#MRQB1wvpVZs
 // solution: https://goplay.space/#4afbasdPvvf
 func main() {
 	fmt.Println("draw mode")
